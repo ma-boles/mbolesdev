552bc287b1c224c2073c4796e9ddb80889c2e6db
ma-boles <ma_boles@hotmail.com>
2024-10-19 17:19:34 -0700

    git commit -m "new file"

---
+++ src/app/components/BackToTopButton.js
@@ -0,0 +1,45 @@
+'use client'
+import { useEffect, useState } from "react"
+import Image from "next/image";
+
+export default function BackToTopButton () {
+    const [isVisible, setIsVisible] = useState(false);
+
+    const toggleVisibility = () => {
+        if(window.scrollY > 300) {
+            setIsVisible(true);
+        } else {
+            setIsVisible(false);
+        }
+    };
+
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth',
+        });
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', toggleVisibility);
+        return () => window.removeEventListener('scroll', toggleVisibility);
+    }, []);
+
+    return(
+        <div>
+            { isVisible && (
+                <button
+                onClick={scrollToTop}
+                className="fixed bottom-4 right-4 p-3 border border-purple-600 rounded-lg hover:bg-purple-600">
+                    <div className="invert">
+                    <Image 
+                    src="/images/arrow-up.svg"
+                    width={20}
+                    height={20}
+                    />
+                    </div>
+                    </button>
+            )} 
+        </div>
+    )
+}
