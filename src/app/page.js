ffc30f1f3eef00f3c73855d2603fa146b852e796
ma-boles <ma_boles@hotmail.com>
2024-10-22 17:26:30 -0700

    git commit -m "updated tailwind styles"

--- src/app/page.js
+++ src/app/page.js
@@ -7,23 +7,23 @@ export default function Home() {
   return (
     <>
     <section className="grid grid-rows-[20px_1fr_20px] min-h-screen py-16 px-8 sm:p-20">
-      <main className="flex flex-col gap-4 items-center sm:items-start text-center sm:text-left">
-          <h1 className="text-5xl font-extrabold">Software Engineer</h1>
+      <div className="flex flex-col gap-4 items-center sm:items-start text-center sm:text-left">
+          <h2>Software Engineer</h2>
           <div className="flex gap-4 items-center flex-col sm:flex-row">
-            <h2 className="text-4xl text-purple-300 font-extrabold">Creating innovative user-centric solutions</h2>
+            <h3 className="text-purple-300">Creating innovative user-centric solutions</h3>
           </div>
-        </main>
-        <div className="row-start-4 flex flex-col items-center sm:flex-row sm:items-start sm:justify-between">
-          <h1 className="mt-36 text-7xl font-extrabold text-purple-700 bg-white whitespace-nowrap">Mary Boles</h1>
+      </div>
+      <div className="row-start-4 flex flex-col items-center sm:flex-row sm:items-start sm:justify-between">
+          <h1 className="mt-36 text-purple-700 bg-white whitespace-nowrap">Mary Boles</h1>
           <div /* nav buttons */ className="flex lg:flex-col gap-2 m-4 md:flex-col sm:flex-row">
             <Link href="#about">
-              <button className="flex-1 px-4 py-4 w-40 text-xl font-semibold bg-purple-950 border-2 border-purple-900 border-opacity-40 rounded-md  transition-colors duration-300 ease-in-out hover:bg-purple-800">About</button>
+              <button className="custom-button bg-purple-950 border-2 border-purple-900 border-opacity-40 hover:bg-purple-800">About</button>
             </Link>
             <Link href="#works">
-              <button className="flex-1 px-4 py-4 s w-40 text-xl font-semibold bg-purple-700 border-2 border-purple-700 rounded-md transition-colors duration-300 ease-in-out hover:bg-purple-600">View Works</button>
+              <button className="custom-button bg-purple-700 border-2 border-purple-700 hover:bg-purple-600">View Works</button>
             </Link>
             <Link href="#contact">
-              <button className="flex-1 py-4 w-40 text-xl font-semibold border-2 border-white rounded-md transition-colors duration-300 ease-in-out hover:bg-purple-500">Get In Touch</button>
+              <button className="custom-button border-2 border-white  hover:bg-purple-500 ">Get In Touch</button>
             </Link>
           </div>
         </div>
@@ -32,19 +32,17 @@ export default function Home() {
     <BackToTopButton />
 
     <section className="grid grid-rows-[20px_1fr_20px] min-h-screen w-full bg-white bg-opacity-20 py-16 px-8  sm:p-20" id="about">
-      <h1 className="font-syne text-5xl font-extrabold">ABOUT | <span className="text-purple-500">SKILLS</span></h1>
+      <h2 className="font-syne">ABOUT | <span className="text-purple-500">SKILLS</span></h2>
       <div className="flex flex-col md:flex-row justify-start mt-20 md:justify-between items-center md-items-start">
         <div className="w-full md:w-2/3 pb-4">
-          <h2 className="mr-2 text-lg font-normal">Hi! I'm Mary, a software engineer transitioning from a career in the education field. Movtivated by my passion for creativity and <span className="font-bold">problem-solving,</span> I decided to pursue development focusing on building impactful web applications.<br/><br/>
-
+          <p className="mr-2 text-lg font-normal">Hi! I'm Mary, a software engineer transitioning from a career in the education field. Movtivated by my passion for creativity and <span className="font-bold">problem-solving,</span> I decided to pursue development focusing on building impactful web applications.<br/><br/>
           With a experience in <span className="font-bold">JavaScript, React, Next.js, Tailwind CSS,</span> I create solutions that merge functionality and clean UI design. My background in education has honed my skills in communication, project management, analytical thinking, which are invaluable to understanding user needs and collaborating with teams.<br/><br/>
-
           Outside of coding, I spend my time enjoying DJing, collecting vinyl records + CD's, watching films and sports, and listening to podcasts!<br/><br/>
           Feel free to get in touch if you'd like to collaborate, or simply <a href="#contact"><span className="underline hover:text-purple-400">connect</span></a>!
-          </h2>
+          </p>
         </div>
-        <div className="w-full p-8 md:w-1/4 min-w-[200px] bg-white bg-opacity-10 rounded-lg ">
-        <h2 className="py-4 text-2xl font-extrabold text-center"><span className="text-purple-500">TOOL</span>BOX</h2>
+        <div className="w-full p-8 md:w-1/4 min-w-[200px] bg-white bg-opacity-5 rounded-lg ">
+        <h4 className="my-2 py-2 bg-purple-500 bg-opacity-50 text-center">TOOLBOX</h4>
         <div className="flex flex-wrap justify-center">
           <img src="https://img.shields.io/badge/Next.js-000000?style=for-the-badge&logo=next.js&logoColor=white" alt="Next.js badge" className="p-1"/>
           <img src="https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black" alt="JavaScript badge" className="p-1"/>
@@ -60,10 +58,10 @@ export default function Home() {
     </section>
 
     <section className="grid grid-rows-[20px_1fr_20px]  min-h-screen py-16 px-8 sm:p-20 " id="works">
-     <h1 className="font-syne text-5xl font-extrabold">W<span className="text-purple-700">O</span>RKS</h1>
+     <h2 className="font-syne">W<span className="text-purple-700">O</span>RKS</h2>
       <div className="px-4 mt-20 flex flex-col bg-white bg-opacity-5 rounded-md sm:flex-row">
           <div className="my-auto">
-            <p className="p-6 text-5xl text-center"><span className="text-green-600 font-extrabold">flow</span>mode</p>
+            <h2 className="p-6 text-center"><span className="text-green-600 font-extrabold">flow</span>mode</h2>
           </div>
           <div className="p-4">
             <p className="mt-12 text-xl">Flowmode is a web-based timer that integrates with Spotify to help users maintain productivity while preventing burnout. By encouraging regular breaks through customizable Flow and Rest intervals, <span className="text-green-600 font-extrabold">flow</span>mode helps users work for longer periods without fatigue. Users can choose audio to accompany both Flow and Rest phases, making the experience enjoyable and effective.
@@ -85,15 +83,17 @@ export default function Home() {
     </section>
 
     <section className="grid grid-rows-[20px_1fr_20px] bg-white bg-opacity-20 min-h-screen relative py-16 px-8 sm:p-20" id="contact">
-      <h1 className="p-1 text-7xl font-extrabold sm:items-start text-center sm:text-left">GET IN <span className="text-purple-500">TOUCH</span><br/>
-      <span className="text-4xl font-bold">I'm Excited to Bring My Skills to Your Team!</span></h1>
+      <div className="sm:items-start text-center sm:text-left">
+      <h1 className="p-1">GET IN <span className="text-purple-500">TOUCH</span></h1><br/>
+      <h3>I'm Excited to Bring My Skills to Your Team!</h3>
+      </div>
       <div className="absolute bottom-0 left-0 right-0 flex p-20 justify-between">
         <div className="text-left">
-          <h2 className="text-2xl font-extrabold text-purple-400">EMAIL</h2>
+          <h4 className="text-purple-400">EMAIL</h4>
           <p className="text-lg font-semibold">email@gmail.com</p>
         </div>
         <div className="text-left">
-          <h2 className="pb-1 text-2xl font-extrabold text-purple-400">SOCIAL</h2>
+          <h4 className="pb-1 text-purple-400">SOCIAL</h4>
           <div className="flex px-1 justify-between">
             <a href="https://www.linkedin.com/in/mary-boles" 
               target="_blank"
