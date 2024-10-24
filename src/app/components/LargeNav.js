62d1f3d1cf249c5f4f515815f7c1eab9d4ae624a
ma-boles <ma_boles@hotmail.com>
2024-10-24 16:03:33 -0700

    git commit -m "added styles for light mode"

--- src/app/components/LargeNav.js
+++ src/app/components/LargeNav.js
@@ -6,13 +6,29 @@ export default function LargeNav({ className }) {
         <>
           <div /* nav buttons */ className={`hidden md:flex flex-col gap-2 m-4 ${className}`}>
           <Link href="#about">
-              <button className="custom-button bg-purple-950 border-2 border-purple-900 border-opacity-40 hover:bg-purple-800">About</button>
+              <button className="custom-button 
+              bg-blue-800 
+              border-blue-700
+              hover:bg-blue-700
+              dark:bg-purple-950 border-2
+              dark:border-purple-900 
+              border-opacity-40 
+              dark:hover:bg-purple-800">About</button>
             </Link>
             <Link href="#works">
-              <button className="custom-button bg-purple-700 border-2 border-purple-700 hover:bg-purple-600">View Works</button>
+              <button className="custom-button 
+              bg-blue-600 
+              border-blue-600
+              hover:bg-blue-500
+              dark:bg-purple-700 border-2 
+              dark:border-purple-700 
+              dark:hover:bg-purple-600">View Works</button>
             </Link>
             <Link href="#contact">
-              <button className="custom-button border-2 border-white  hover:bg-purple-500 ">Get In Touch</button>
+              <button className="custom-button 
+              border-2 border-white  
+              hover:bg-blue-500
+              dark:hover:bg-purple-500 ">Get In Touch</button>
             </Link>
           </div>
         </>
