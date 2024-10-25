0dba041618aa1dba0475e455247ba6c068777ada
ma-boles <ma_boles@hotmail.com>
2024-10-24 17:41:03 -0700

    git commit -m "added dark mode"

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -7,10 +8,6 @@ module.exports = {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
       fontFamily: {
         syne: ['Syne', 'sans-serif'],
       }
