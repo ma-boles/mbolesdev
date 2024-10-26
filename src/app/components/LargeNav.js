626bd790ed53470903ad43adc34dc7a2d36b1180
ma-boles <ma_boles@hotmail.com>
2024-10-25 18:08:46 -0700

    git commit -m "changed light mode colors"

--- src/app/components/LargeNav.js
+++ src/app/components/LargeNav.js
@@ -7,9 +7,9 @@ export default function LargeNav({ className }) {
           <div /* nav buttons */ className={`hidden md:flex flex-col gap-2 m-4 ${className}`}>
           <Link href="#about">
               <button className="custom-button 
-              bg-blue-800 
-              border-blue-700
-              hover:bg-blue-700
+              bg-gray-800 
+              border-gray-700
+              hover:bg-gray-700
               dark:bg-purple-950 border-2
               dark:border-purple-900 
               border-opacity-40 
@@ -17,9 +17,9 @@ export default function LargeNav({ className }) {
             </Link>
             <Link href="#works">
               <button className="custom-button 
-              bg-blue-600 
-              border-blue-600
-              hover:bg-blue-500
+              bg-gray-600 
+              border-gray-600
+              hover:bg-gray-500
               dark:bg-purple-700 border-2 
               dark:border-purple-700 
               dark:hover:bg-purple-600">View Works</button>
@@ -27,7 +27,7 @@ export default function LargeNav({ className }) {
             <Link href="#contact">
               <button className="custom-button 
               border-2 border-white  
-              hover:bg-blue-500
+              hover:bg-gray-500
               dark:hover:bg-purple-500 ">Get In Touch</button>
             </Link>
           </div>
--- src/app/components/SmallNav.js
+++ src/app/components/SmallNav.js
@@ -7,18 +7,18 @@ export default function SmallNav({ className }) {
           <div /* nav buttons */ className={`flex mt-2 w-full bg-white bg-opacity-5 justify-between ${className}`}>
             <Link href="#about" className="flex-1">
               <button className="custom-nav w-full
-              hover:bg-blue-700 text-center
+              hover:bg-gray-700 text-center
               dark:hover:bg-purple-600
               ">About</button>
             </Link>
             <Link href="#works" className="flex-1">
               <button className="custom-nav w-full
-              hover:bg-blue-700 text-center
+              hover:bg-gray-700 text-center
               dark:hover:bg-purple-600">Works</button>
             </Link>
             <Link href="#contact" className="flex-1">
               <button className="custom-nav w-full
-              hover:bg-blue-700 text-center
+              hover:bg-gray-700 text-center
               dark:hover:bg-purple-600">Contact</button>
             </Link>
           </div>
