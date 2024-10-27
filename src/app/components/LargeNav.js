6b9767701c8ba08a609120444dbf193ddd1d7328
ma-boles <ma_boles@hotmail.com>
2024-10-27 14:45:30 -0700

    git commit -m "updated button hover styles"

--- src/app/components/LargeNav.js
+++ src/app/components/LargeNav.js
@@ -8,29 +8,37 @@ export default function LargeNav({ className }) {
           <Link href="#about">
               <button className="custom-button 
               bg-gray-800 
-              border-gray-700
+              border-gray-800
               hover:bg-gray-700
+              rounded-lg shadow-[0_4px_10px_rgba(100,100,100,0.1)]
               dark:bg-purple-950 border-2
-              dark:border-purple-900 
+              dark:border-purple-950 
               border-opacity-40 
-              dark:hover:bg-purple-800">About</button>
+              dark:hover:bg-purple-800
+              hover:scale-105 transition-transform duration-200 ease-in-out focus:outline-none focus:ring-4 focus:ring-purple-500
+              ">About</button>
             </Link>
             <Link href="#works">
               <button className="custom-button 
               bg-gray-600 
               border-gray-600
               hover:bg-gray-500
+                rounded-lg shadow-lg
               dark:bg-purple-700 border-2 
               dark:border-purple-700 
-              dark:hover:bg-purple-600">View Works</button>
+              dark:hover:bg-purple-600
+              hover:scale-105 transition-transform duration-200 ease-in-out focus:outline-none focus:ring-4 focus:ring-purple-400">View Works</button>
             </Link>
             <Link href="#contact">
               <button className="custom-button 
               border-2 border-white  
               hover:bg-gray-500
-              dark:hover:bg-purple-500 ">Get In Touch</button>
+              dark:hover:bg-purple-500 
+                rounded-lg shadow-lg
+                hover:scale-105 transition-transform duration-200 ease-in-out focus:outline-none focus:ring-4 focus:ring-purple-400"
+                >Get In Touch</button>
             </Link>
           </div>
         </>
     )
-}        {/*  <div /* nav buttons *//* className={`flex lg:flex-col gap-2 m-4 md:flex-col sm:flex-row`}>*/}
+}
