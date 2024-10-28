3ca2849305bc969f86473b8c11292c880c09d21d
ma-boles <ma_boles@hotmail.com>
2024-10-28 14:57:35 -0700

    git commit -m "updated text size for li elements"

--- src/app/components/ProjectDetails.js
+++ src/app/components/ProjectDetails.js
@@ -16,32 +16,32 @@ export default function ProjectDetails () {
         </button>
         {isExpanded && (
        <div className="pt-4">
-        <ul><span className="font-bold text-lg">Features</span>
-            <li className="text-base"><span className="font-bold">Customizable Timer</span>: Set personalized Flow and Rest intervals to sustain productivity.</li>
-            <li className="text-base"><span className="font-bold">Spotify Integration:</span> Play different audio during Flow and Rest periods, creating an engaging and balanced workflow.</li>
-            <li className="text-base"><span className="font-bold">Multiple Cycles:</span> Manage multiple Flow/Rest cycles with real-time tracking to maintain a consistent rhythm.</li>
-            <li className="text-base"><span className="font-bold">User Profiles:</span> Build personalized accounts for saving settings and favorite playlists.</li>
+        <ul><span className="font-bold text-lg underline">Features</span>
+            <li className="text-lg"><span className="font-bold">Customizable Timer</span>: Set personalized Flow and Rest intervals to sustain productivity.</li>
+            <li className="text-lg"><span className="font-bold">Spotify Integration:</span> Play different audio during Flow and Rest periods, creating an engaging and balanced workflow.</li>
+            <li className="text-lg"><span className="font-bold">Multiple Cycles:</span> Manage multiple Flow/Rest cycles with real-time tracking to maintain a consistent rhythm.</li>
+            <li className="text-lg"><span className="font-bold">User Profiles:</span> Build personalized accounts for saving settings and favorite playlists.</li>
           </ul><br/>
 
-          <ul><span className="font-bold text-lg">Tech Stack</span>
-            <li className="text-base"><span className="font-bold">Languages & Frameworks:</span> JavaScript, React, Next.js, Tailwind CSS</li>
-            <li className="text-base"><span className="font-bold">APIs:</span> Spotify Web Playback SDK</li>
-            <li className="text-base"><span className="font-bold">Database:</span> MongoDB</li>
+          <ul><span className="font-bold text-lg underline">Tech Stack</span>
+            <li className="text-lg"><span className="font-bold">Languages & Frameworks:</span> JavaScript, React, Next.js, Tailwind CSS</li>
+            <li className="text-lg"><span className="font-bold">APIs:</span> Spotify Web Playback SDK</li>
+            <li className="text-lg"><span className="font-bold">Database:</span> MongoDB</li>
           </ul><br/>
 
-          <ul><span className="font-bold text-lg">Tools</span>
-            <li className="text-base"><span className="font-bold">Version Control:</span> Git</li>
-            <li className="text-base"><span className="font-bold">Deployment:</span> Vercel</li>
+          <ul><span className="font-bold text-lg underline">Tools</span>
+            <li className="text-lg"><span className="font-bold">Version Control:</span> Git</li>
+            <li className="text-lg"><span className="font-bold">Deployment:</span> Vercel</li>
           </ul><br/>
 
-          <ul><span className="font-bold text-lg">In Progress</span>
-            <li className="text-base"><span className="font-bold">Shuffle Feature:</span> Adding functionality to prevent track repetition during cycles.</li>
-            <li className="text-base"><span className="font-bold">Customizable Templates:</span> Allowing users to create and save custom templates for easy setup with once click.</li>
+          <ul><span className="font-bold text-lg underline">In Progress</span>
+            <li className="text-lg"><span className="font-bold">Shuffle Feature:</span> Adding functionality to prevent track repetition during cycles.</li>
+            <li className="text-lg"><span className="font-bold">Customizable Templates:</span> Allowing users to create and save custom templates for easy setup with one click.</li>
           </ul><br/>
 
-          <ul><span className="text-lg font-bold">Future Enhancements</span>
-            <li className="text-base"><span className="font-bold">Responsive Design:</span> Further optimization for both desktop and mobile devices.</li>
-            <li className="text-base"><span className="font-bold">UI/UX Improvements:</span> Refining the interface for a smoother and more intuitive experience across devices.</li>
+          <ul><span className="text-lg font-bold underline">Future Enhancements</span>
+            <li className="text-lg"><span className="font-bold">Responsive Design:</span> Further optimization for both desktop and mobile devices.</li>
+            <li className="text-lg"><span className="font-bold">UI/UX Improvements:</span> Refining the interface for a smoother and more intuitive experience across devices.</li>
           </ul><br/>
           </div>
            )}
