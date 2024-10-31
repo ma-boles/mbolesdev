912b9231921cc61c0e839531d9a1f1e3a9810b22
ma-boles <ma_boles@hotmail.com>
2024-10-30 17:19:51 -0700

    git commit -m "removed whitespace"

--- src/app/components/BackToTopButton.js
+++ src/app/components/BackToTopButton.js
@@ -27,25 +27,25 @@ export default function BackToTopButton () {
 
     return(
         <div>
-            { isVisible && (
+            {isVisible && (
                 <button
                 onClick={scrollToTop}
-                className="fixed bottom-3 right-3 p-2 border border-white rounded-lg 
+                className="fixed bottom-3 right-3 p-2 border border-white rounded-lg
                 hover:bg-slate-700
-                dark:border-purple-600 
+                dark:border-purple-600
                 dark:hover:bg-purple-600"
                 style={{ zIndex: 1000 }}>
                     <div className="invert">
-                    <Image 
+                    <Image
                     src="/images/arrow-up.svg"
                     alt="Arrow up"
-                    width={20}       
-                    height={20}     
+                    width={20}
+                    height={20}
                     className="w-3 h-5 sm:w-3 sm:h-5 md:w-8 md:h-10 lg:w-8 lg:h-10 object-contain"
                     />
                     </div>
                     </button>
-            )} 
+            )}
         </div>
     )
 }
