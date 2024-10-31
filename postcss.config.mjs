585617bf74e5571afa338948923621933d2cb559
ma-boles <ma_boles@hotmail.com>
2024-10-30 18:07:43 -0700

    git commit -m "added autoprefixer"

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,13 @@
+import autoprefixer from 'autoprefixer';
+
 /** @type {import('postcss-load-config').Config} */
 const config = {
-  plugins: {
-    tailwindcss: {},
-  },
+  plugins: [
+    //tailwindcss: {},
+    //autoprefixer: {},
+    require('tailwind')('./tailwind.config.js'),
+    require('autoprefixer')
+  ],
 };
 
 export default config;
