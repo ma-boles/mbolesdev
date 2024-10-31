5f47dad6f7c873c4002aa8664e896585724c5a27
ma-boles <ma_boles@hotmail.com>
2024-10-31 15:39:26 -0700

    git commit -m "added width and height to toolox images"

--- src/app/page.js
+++ src/app/page.js
@@ -66,47 +66,56 @@ export default function Home() {
             src="https://img.shields.io/badge/Next.js-000000?style=for-the-badge&logo=next.js&logoColor=white" 
             alt="Next.js badge" 
             className="p-1"
-            style={{ width: 'auto', height: 'auto' }}/>
+            width={99}
+            height={28}/>
             <Image 
             src="https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black" 
             alt="JavaScript badge" 
             className="p-1"
-            style={{ width: 'auto', height: 'auto' }}/>
+            width={99}
+            height={28}/>
             <Image 
             src="https://img.shields.io/badge/React-61DAFB?style=for-the-badge&logo=react&logoColor=black" 
             alt="React badge" 
             className="p-1"
-            style={{ width: 'auto', height: 'auto' }}/>
+            width={99}
+            height={28}/>
             <Image 
             src="https://img.shields.io/badge/Tailwind_CSS-06B6D4?style=for-the-badge&logo=tailwind-css&logoColor=white" 
             alt="Tailwind CSS badge" 
             className="p-1"
-            style={{ width: 'auto', height: 'auto' }}/>
+            width={99}
+            height={28}/>
             <Image 
             src="https://img.shields.io/badge/APIs-000000?style=for-the-badge&logo=api&logoColor=white" 
             alt="APIs badge" 
             className="p-1"
-            style={{ width: 'auto', height: 'auto' }}/>
+            width={99}
+            height={28}/>
             <Image 
             src="https://img.shields.io/badge/GitHub-181717?style=for-the-badge&logo=github&logoColor=white" 
             alt="GitHub badge" 
             className="p-1"
-            style={{ width: 'auto', height: 'auto' }}/>
+            width={99}
+            height={28}/>
             <Image 
             src="https://img.shields.io/badge/VS_Code-007ACC?style=for-the-badge&logo=visual-studio-code&logoColor=white" 
             alt="VS Code badge" 
             className="p-1"
-            style={{ width: 'auto', height: 'auto' }}/>
+            width={99}
+            height={28}/>
             <Image 
             src="https://img.shields.io/badge/MongoDB-47A248?style=for-the-badge&logo=mongodb&logoColor=white" 
             alt="MongoDB badge" 
             className="p-1"
-            style={{ width: 'auto', height: 'auto' }}/>
+            width={99}
+            height={28}/>
             <Image 
             src="https://img.shields.io/badge/Vercel-000000?style=for-the-badge&logo=vercel&logoColor=white" 
             alt="Vercel badge" 
             className="p-1"
-            style={{ width: 'auto', height: 'auto' }}/>
+            width={99}
+            height={28}/>
           </div>
       </div>
     </div>
