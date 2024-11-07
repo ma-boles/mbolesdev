5922d33032e95c60e25cfb52663f08771ea83539
ma-boles <ma_boles@hotmail.com>
2024-11-07 15:06:07 -0800

    git commit -m "added code for custom text outline for safari browser"

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,5 +23,46 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    function({ addUtilities, theme }) {
+      addUtilities({
+        '.safari-text-outline-white': {
+          color: theme('colors.white'),
+          '-webkit-text-stroke': `1px ${theme('colors.white')}`,
+        },
+        '.safari-text-outline-purple-400': {
+          color: theme('colors.purple.400'),
+          '-webkit-text-stroke': `1px ${theme('colors.purple.400')}`,
+        },
+        '.safari-text-outline-purple-500': {
+          color: theme('colors.purple.500'),
+          '-webkit-text-stroke': `1px ${theme('colors.purple.500')}`,
+        },
+        '.safari-text-outline-purple-700': {
+          color: theme('colors.purple.700'),
+          '-webkit-text-stroke': `1px ${theme('colors.purple.700')}`,
+        },
+        '.safari-text-outline-gray-300': {
+          color: theme('colors.gray.300'),
+          '-webkit-text-stroke': `1px ${theme('colors.gray.300')}`,
+        },
+        '.safari-text-outline-gray-400': {
+          color: theme('colors.gray.400'),
+          '-webkit-text-stroke': `1px ${theme('colors.gray.400')}`,
+        },
+        '.safari-text-outline-gray-600': {
+          color: theme('colors.gray.600'),
+          '-webkit-text-stroke': `1px ${theme('colors.gray.600')}`,
+        },
+        '.safari-text-outline-gray-700': {
+          color: theme('colors.gray.700'),
+          '-webkit-text-stroke': `1px ${theme('colors.gray.700')}`,
+        },
+        '.safari-text-outline-green-600': {
+          color: theme('colors.green.600'),
+          '-webkit-text-stroke': `1px ${theme('colors.green.600')}`,
+        },
+      }, ['responsive']);
+    },
+  ],
 };
