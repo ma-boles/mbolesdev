aa7d09d5b72d4f187ae25dae26ee318d31cf3e20
ma-boles <ma_boles@hotmail.com>
2024-11-07 18:01:55 -0800

    git commit -m "moved viewport"

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,18 @@
 import "@/app/styles/globals.css"
+import Head from "next/head";
 
 export const metadata = {
   title: "Mary Boles | Developer Portfolio",
   description: "Portfolio showcasing my web development projects, skills, and experience in building modern applications. Built in Next.js and Tailwind CSS.",
   keywords: ["Web Developer"," Portfolio", "Next.js", "Tailwind CSS", "JavaScript", "React"],
-  viewport: 'width=device-width, initial-scale=1'
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <body>
         {children}
       </body>
