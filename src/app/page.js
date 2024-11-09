6744daccb7619958cc18a4e2a4878bb2c428a010
ma-boles <ma_boles@hotmail.com>
2024-11-08 16:10:18 -0800

    git commit -m "updated landing section lg-mobile  margins"

--- src/app/page.js
+++ src/app/page.js
@@ -39,7 +39,7 @@ export default function Home() {
             <div className="flex gap-4 items-center flex-col sm:flex-row">
               <h3 className="text-gray-300 safari-text-outline-gray-300 font-extrabold dark:text-purple-400 dark:safari-text-outline-purple-400 fade-in-up-2" >Delivering innovative user-centric solutions</h3>
             </div>
-            <h1 /* mobile screen */ className="px-2 mt-16 lg-mobile:mt-8 text-gray-700 safari-text-outline-gray-700 font-extrabold dark:text-purple-700 dark:safari-text-outline-purple-700 bg-white whitespace-nowrap md:hidden  fade-in-up-3">Mary Boles</h1>
+            <h1 /* mobile screen */ className="px-2 mt-8 lg-mobile:mt-24 text-gray-700 safari-text-outline-gray-700 font-extrabold dark:text-purple-700 dark:safari-text-outline-purple-700 bg-white whitespace-nowrap md:hidden  fade-in-up-3">Mary Boles</h1>
         </div>
         <div className="row-start-4 flex flex-col items-center sm:flex-row sm:items-start justify-between z-10">
           <h1 /* med screen */ className="mt-36 px-2 text-gray-700  safari-text-outline-gray-700 font-extrabold dark:text-purple-700 dark:safari-text-outline-purple-700 bg-white whitespace-nowrap hidden md:block shadow-[0px_12px_20px_rgba(0,0,0,0.5)] fade-in-up-3">Mary Boles</h1>
