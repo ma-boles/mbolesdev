d5d400335e456211210700e43e2d0beadd5ac63a
Mary Boles <mboles.dev@gmail.com>
2025-09-22 16:52:02 -0700

    git commit -m "Removed Head tags"

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,18 +1,15 @@
 import "@/app/styles/globals.css"
-import Head from "next/head";
 
 export const metadata = {
   title: "Mary Boles | Developer Portfolio",
   description: "Portfolio showcasing my web development projects, skills, and experience in building modern applications. Built in Next.js and Tailwind CSS.",
   keywords: ["Web Developer"," Portfolio", "Next.js", "Tailwind CSS", "JavaScript", "React"],
+  viewpoint: ["width=device-width, initial-scale=1"]
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
       <body>
         {children}
       </body>
